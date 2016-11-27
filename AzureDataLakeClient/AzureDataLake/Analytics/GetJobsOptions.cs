4aef503c1601c762c661693ac675a473358a63d8
Saveen Reddy <saveenr@live.com>
2016-11-27 13:54:13 -0800

    Moved class to separate file

--- AzureDataLakeClient/AzureDataLake/Analytics/GetJobsOptions.cs
+++ AzureDataLakeClient/AzureDataLake/Analytics/GetJobsOptions.cs
@@ -1,29 +1,5 @@
 namespace AzureDataLakeClient.Analytics
 {
-    public class JobListSorting
-    {
-        public AzureDataLakeClient.OData.ExprField Field;
-        public OrderByDirection Direction;
-        
-        public string CreateOrderByString()
-        {
-            if (this.Field != null)
-            {
-                var dir = DirectionToString(this.Direction);
-                string orderBy = string.Format("{0} {1}", this.Field.Name, dir);
-                return orderBy;
-            }
-
-            return null;
-        }
-
-        private static string DirectionToString(OrderByDirection direction)
-        {
-            var dir = (direction == OrderByDirection.Ascending) ? "asc" : "desc";
-            return dir;
-        }
-    }
-
     public class GetJobsOptions
     {
         public int Top=0; // 300 is the ADLA limit
