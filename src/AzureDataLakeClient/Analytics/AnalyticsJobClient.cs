8ee4e991cb88c2203d061057e361af5e815621b8
Saveen Reddy <saveenr@live.com>
2017-03-11 18:02:33 -0800

    Moved class to its own file

--- src/AzureDataLakeClient/Analytics/AnalyticsJobClient.cs
+++ src/AzureDataLakeClient/Analytics/AnalyticsJobClient.cs
@@ -1,94 +1,9 @@
 using System.Collections.Generic;
 using AzureDataLakeClient.Authentication;
-using Microsoft.Azure.Management.DataLake.Analytics;
 using ADL=Microsoft.Azure.Management.DataLake;
 
 namespace AzureDataLakeClient.Analytics
 {
-    public class AnalyticsJobsRestClient
-    {
-        private ADL.Analytics.DataLakeAnalyticsJobManagementClient _client;
-        private Microsoft.Rest.ServiceClientCredentials _creds;
-
-        public AnalyticsJobsRestClient(Microsoft.Rest.ServiceClientCredentials creds)
-        {
-            this._creds = creds;
-            this._client = new ADL.Analytics.DataLakeAnalyticsJobManagementClient(this._creds);
-        }
-
-        public ADL.Analytics.Models.JobInformation JobGet(string account, System.Guid jobid)
-        {
-            var job = this._client.Job.Get(account, jobid);
-            return job;
-        }
-
-        public IEnumerable<ADL.Analytics.Models.JobInformation> JobList(string account,
-            Microsoft.Rest.Azure.OData.ODataQuery<ADL.Analytics.Models.JobInformation> odata_query, int top)
-        {
-            // Other parameters
-            string opt_select = null;
-            bool? opt_count = null;
-
-            int item_count = 0;
-            var page = this._client.Job.List(account, odata_query, opt_select, opt_count);
-            foreach (
-                var job in
-                RESTUtil.EnumItemsInPages<ADL.Analytics.Models.JobInformation>(page,
-                    p => this._client.Job.ListNext(p.NextPageLink)))
-            {
-                yield return job;
-                item_count++;
-
-                if ((top > 0) && (item_count >= top))
-                {
-                    break;
-                }
-            }
-
-        }
-
-        public ADL.Analytics.Models.JobInformation JobCreate(string account, SubmitJobOptions options)
-        {
-            var parameters = CreateNewJobProperties(options);
-            var job_info = this._client.Job.Create(account, options.JobID, parameters);
-
-            return job_info;
-
-        }
-
-        private static ADL.Analytics.Models.JobInformation CreateNewJobProperties(SubmitJobOptions options)
-        {
-            var jobprops = new ADL.Analytics.Models.USqlJobProperties();
-            jobprops.Script = options.ScriptText;
-
-            var jobType = ADL.Analytics.Models.JobType.USql;
-            int priority = 1;
-            int dop = 1;
-
-            var parameters = new ADL.Analytics.Models.JobInformation(
-                name: options.JobName,
-                type: jobType,
-                properties: jobprops,
-                priority: priority,
-                degreeOfParallelism: dop,
-                jobId: options.JobID);
-            return parameters;
-        }
-
-        public ADL.Analytics.Models.JobStatistics GetStatistics(string account, System.Guid jobid)
-        {
-            var stats = this._client.Job.GetStatistics(account, jobid);
-            return stats;
-        }
-
-        public ADL.Analytics.Models.JobDataPath GetDebugDataPath(string account, System.Guid jobid)
-        {
-            var jobdatapath = this._client.Job.GetDebugDataPath(account, jobid);
-            return jobdatapath;
-        }
-
-    }
-
     public class AnalyticsJobClient : AccountClientBase
     {
         // The maximum page size for ADLA list is 300
