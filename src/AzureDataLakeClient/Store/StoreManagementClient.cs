f75d293097e6366f68ebde03e520781c96c606fa
Saveen Reddy <saveenr@live.com>
2017-03-11 23:59:29 -0800

    Internal rename

--- src/AzureDataLakeClient/Store/StoreManagementClient.cs
+++ src/AzureDataLakeClient/Store/StoreManagementClient.cs
@@ -7,22 +7,22 @@ namespace AzureDataLakeClient.Store
 {
     public class StoreManagementClient : ClientBase
     {
-        private ADL.Store.DataLakeStoreAccountManagementClient _adls_mgmt_rest_client;
+        private ADL.Store.DataLakeStoreAccountManagementClient _rest_client;
         private Subscription Sub;
 
         public StoreManagementClient(Subscription sub, AuthenticatedSession authSession) :
             base(authSession)
         {
             this.Sub = sub;
-            this._adls_mgmt_rest_client = new ADL.Store.DataLakeStoreAccountManagementClient(this.AuthenticatedSession.Credentials);
-            this._adls_mgmt_rest_client.SubscriptionId = sub.ID;
+            this._rest_client = new ADL.Store.DataLakeStoreAccountManagementClient(this.AuthenticatedSession.Credentials);
+            this._rest_client.SubscriptionId = sub.ID;
         }
 
         public IEnumerable<ADL.Store.Models.DataLakeStoreAccount> ListAccounts()
         {
-            var page = this._adls_mgmt_rest_client.Account.List();
+            var page = this._rest_client.Account.List();
             foreach (var acc in RESTUtil.EnumItemsInPages(page,
-                p => this._adls_mgmt_rest_client.Account.ListNext(p.NextPageLink)))
+                p => this._rest_client.Account.ListNext(p.NextPageLink)))
             {
                 yield return acc;
             }
@@ -30,10 +30,10 @@ namespace AzureDataLakeClient.Store
 
         public IEnumerable<ADL.Store.Models.DataLakeStoreAccount> ListAccountsByResourceGroup(string resource_group)
         {
-            var page = this._adls_mgmt_rest_client.Account.ListByResourceGroup(resource_group);
+            var page = this._rest_client.Account.ListByResourceGroup(resource_group);
 
             foreach (var acc in RESTUtil.EnumItemsInPages(page,
-                p => this._adls_mgmt_rest_client.Account.ListByResourceGroupNext(p.NextPageLink)))
+                p => this._rest_client.Account.ListByResourceGroupNext(p.NextPageLink)))
             {
                 yield return acc;
             }
@@ -41,17 +41,17 @@ namespace AzureDataLakeClient.Store
 
         public ADL.Store.Models.DataLakeStoreAccount GetAccount(StoreAccountRef account)
         {
-            return this._adls_mgmt_rest_client.Account.Get(account.ResourceGroup, account.Name);
+            return this._rest_client.Account.Get(account.ResourceGroup, account.Name);
         }
 
         public void Update(StoreAccountRef account, ADL.Store.Models.DataLakeStoreAccountUpdateParameters parameters)
         {
-            this._adls_mgmt_rest_client.Account.Update(account.ResourceGroup, account.Name, parameters);
+            this._rest_client.Account.Update(account.ResourceGroup, account.Name, parameters);
         }
 
         public void Delete(StoreAccountRef account)
         {
-            this._adls_mgmt_rest_client.Account.Delete(account.ResourceGroup, account.Name);
+            this._rest_client.Account.Delete(account.ResourceGroup, account.Name);
         }
 
     }
