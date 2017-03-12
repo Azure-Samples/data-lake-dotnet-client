06fdf67f872c4d8f6122d5a5dc64322b6dd888b3
Saveen Reddy <saveenr@live.com>
2017-03-12 08:05:14 -0700

    Moved class to its own file

---
+++ src/AzureDataLakeClient/Analytics/AnalyticsAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace AzureDataLakeClient.Analytics
+{
+    public class AnalyticsAccount
+    {
+        public string Name;
+        public AzureDataLakeClient.Rm.Subscription Subscription;
+        public AzureDataLakeClient.Rm.ResourceGroup ResourceGroup;
+
+        public AnalyticsAccount(string name, AzureDataLakeClient.Rm.Subscription sub, AzureDataLakeClient.Rm.ResourceGroup rg)
+        {
+            this.Name = name;
+            this.Subscription = sub;
+            this.ResourceGroup = rg;
+        }
+
+        public AnalyticsAccountUri GetUri()
+        {
+            return new AnalyticsAccountUri(this.Name);
+        }
+    }
+}
--- src/AzureDataLakeClient/Analytics/AnalyticsAccountRef.cs
+++ src/AzureDataLakeClient/Analytics/AnalyticsAccountRef.cs
@@ -1,23 +1,5 @@
 namespace AzureDataLakeClient.Analytics
 {
-    public class AnalyticsAccount
-    {
-        public string Name;
-        public AzureDataLakeClient.Rm.Subscription Subscription;
-        public AzureDataLakeClient.Rm.ResourceGroup ResourceGroup;
-
-        public AnalyticsAccount(string name, AzureDataLakeClient.Rm.Subscription sub, AzureDataLakeClient.Rm.ResourceGroup rg)
-        {
-            this.Name = name;
-            this.Subscription = sub;
-            this.ResourceGroup = rg;
-        }
-
-        public AnalyticsAccountUri GetUri()
-        {
-            return new AnalyticsAccountUri(this.Name);
-        }
-    }
 
 
     public class AnalyticsAccountRmRef
