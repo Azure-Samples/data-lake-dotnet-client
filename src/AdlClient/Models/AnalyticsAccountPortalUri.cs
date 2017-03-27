d7c74691ac941ea39ec9b9fa356bb69067470768
Saveen Reddy <saveenr@live.com>
2017-03-27 15:21:36 -0700

    Added Analytics account Uri class

--- src/AdlClient/Models/AnalyticsAccountPortalUri.cs
+++ src/AdlClient/Models/AnalyticsAccountPortalUri.cs
@@ -30,5 +30,81 @@
                     this.SubscriptionId, this.ResourceGroup, this.Account);
             return uri;
         }
+
+        public static AnalyticsAccountPortalUri Parse(string s)
+        {
+            var uri = new System.Uri(s);
+
+            var adl_portal_authority = "portal.azure.com";
+            if (uri.Authority.ToLower() != adl_portal_authority)
+            {
+                string msg = string.Format("Malformed job portal uri: Uri.Authority must be with \"{0}\"", adl_portal_authority);
+                throw new System.ArgumentException(msg);
+            }
+
+            string fragment = uri.Fragment;
+
+            string fragment_prefix = "#resource/";
+
+            if (!fragment.StartsWith(fragment_prefix))
+            {
+                throw new System.ArgumentException("invalid uri fragment");
+            }
+
+            var fragment_remainder = fragment.Substring(fragment_prefix.Length);
+            fragment_remainder = System.Uri.UnescapeDataString(fragment_remainder);
+
+            var fragment_tokens = fragment_remainder.Split('/');
+
+            // Basic fragment validation
+
+            if (fragment_tokens.Length < 9)
+            {
+                //throw new System.ArgumentException("Malformed job portal uri: has Fragment has less than 11 parts");
+            }
+
+            // Subscription
+            if (fragment_tokens[0] != "subscriptions")
+            {
+                throw new System.ArgumentException(
+                    "Malformed job portal uri: missing \"subscriptions\" in Fragment at position 0");
+            }
+            var subid = fragment_tokens[1];
+
+            // Resource Group
+            if (fragment_tokens[2] != "resourcegroups")
+            {
+                throw new System.ArgumentException(
+                    "Malformed job portal uri: missing \"resourcegroups\" in Fragment at position 2");
+            }
+            var rg = fragment_tokens[3];
+
+            // Provider
+            if (fragment_tokens[4] != "providers")
+            {
+                throw new System.ArgumentException(
+                    "Malformed job portal uri: missing \"providers\" in Fragment at position 4");
+            }
+
+            if (fragment_tokens[5] != "Microsoft.DataLakeAnalytics")
+            {
+                throw new System.ArgumentException(
+                    "Malformed job portal uri: missing \"Microsoft.DataLakeAnalytics\" in Fragment at position 5");
+            }
+
+            // ADLA Account
+            if (fragment_tokens[6] != "accounts")
+            {
+                throw new System.ArgumentException(
+                    "Malformed job portal uri: missing \"accounts\" in Fragment at position 6");
+            }
+
+            var account_name = fragment_tokens[7];
+
+            // Final URI
+            var account_portal_uri = new AnalyticsAccountPortalUri(subid, rg, account_name);
+            return account_portal_uri;
+        }
+
     }
 }
--- src/TestAdlClient/Analytics/Analytics_JobRef_Tests.cs
+++ src/TestAdlClient/Analytics/Analytics_JobRef_Tests.cs
@@ -6,7 +6,20 @@ namespace TestAdlClient.Analytics
     public class Analytics_JobRef_Tests : Base_Tests
     {
         [TestMethod]
-        public void ParsePortalLink()
+        public void ParseAccountPortalLink()
+        {
+            string s =
+                "https://portal.azure.com/?feature.customportal=false#resource/subscriptions/ace74b35-b0de-428b-a1d9-55459d7a6e30/resourcegroups/adlpminsights/providers/Microsoft.DataLakeAnalytics/accounts/adlpm/overview";
+            var portal_uri = AdlClient.Models.AnalyticsAccountPortalUri.Parse(s);
+
+            Assert.IsNotNull(portal_uri);
+            Assert.AreEqual("ace74b35-b0de-428b-a1d9-55459d7a6e30", portal_uri.SubscriptionId);
+            Assert.AreEqual("adlpminsights", portal_uri.ResourceGroup);
+            Assert.AreEqual("adlpm", portal_uri.Account);
+        }
+
+        [TestMethod]
+        public void ParseJobPortalLink()
         {
             string s =
                 "https://portal.azure.com/?feature.customportal=false#blade/Microsoft_Azure_DataLakeAnalytics/SqlIpJobDetailsBlade/accountId/%2Fsubscriptions%2Face74b35-b0de-428b-a1d9-55459d7a6e30%2Fresourcegroups%2Fadlpminsights%2Fproviders%2FMicrosoft.DataLakeAnalytics%2Faccounts%2Fadlpm/jobId/814e10ca-2e56-4814-8022-5632e19b561c";
