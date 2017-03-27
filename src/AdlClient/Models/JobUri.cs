1c75cc9ca4b5baab14c060d289e062e3df54a306
Saveen Reddy <saveenr@live.com>
2017-03-27 08:04:22 -0700

    Made job Uri parsing more robust

--- src/AdlClient/Models/JobUri.cs
+++ src/AdlClient/Models/JobUri.cs
@@ -13,26 +13,45 @@
 
         public static JobUri Parse(string s)
         {
-            var u = new System.Uri(s);
-            var authority_lc = u.Authority.ToLower();
-            if (authority_lc.EndsWith(".azuredatalakeanalytics.net"))
+            // Example: "https://adlpm.azuredatalakeanalytics.net/jobs/814e10ca-2e56-4814-8022-5632e19b561c?api-version=2016-11-01";
+
+            var uri = new System.Uri(s);
+            var authority_lc = uri.Authority.ToLower();
+
+            var adl_authority_suffix = ".azuredatalakeanalytics.net";
+            if (!authority_lc.EndsWith(adl_authority_suffix))
             {
-                // Example: "https://adlpm.azuredatalakeanalytics.net/jobs/814e10ca-2e56-4814-8022-5632e19b561c?api-version=2016-11-01";
-                var tokens = authority_lc.Split('.');
-
-                string account_name = tokens[0];
-
-
-                string tt = u.LocalPath;
-                var tokens2 = tt.Split('/');
-
-                var jobid_st = tokens2[2];
-
-                var jobid = System.Guid.Parse(jobid_st);
-                var joblink = new JobUri(account_name, jobid);
-                return joblink;
+                string msg = string.Format("Malformed job uri: Uri.Authority should end with \"{0}\"",
+                    adl_authority_suffix);
+                throw new System.ArgumentException(msg);
             }
-            throw new System.ArgumentException("invalid uri authority");
+
+            // Find the account name
+            var authority_tokens = authority_lc.Split('.');
+            string account_name = authority_tokens[0];
+
+            // Break up the path to find the job id
+            var localpath_tokens = uri.LocalPath.Split('/');
+
+            if (localpath_tokens.Length < 3)
+            {
+                throw new System.ArgumentException("Malformed job uri: has LocalPath has less than 3 parts");
+            }
+
+            if (localpath_tokens[0] != "")
+            {
+                throw new System.ArgumentException("Malformed job uri: first token should be empty");
+            }
+
+            if (localpath_tokens[1] != "jobs")
+            {
+                throw new System.ArgumentException("Malformed job uri: missing \"jobs\" in LocalPath");
+            }
+
+            var jobid_str = localpath_tokens[2];
+            var jobid = System.Guid.Parse(jobid_str);
+            var joburi = new JobUri(account_name, jobid);
+            return joburi;
         }
 
 
