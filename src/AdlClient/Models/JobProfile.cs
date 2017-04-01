95a6886f4f3b1c401a9a1fdf2d6450e547709293
Saveen Reddy <saveenr@live.com>
2017-03-31 19:07:08 -0700

    cleanup

--- src/AdlClient/Models/JobProfile.cs
+++ src/AdlClient/Models/JobProfile.cs
@@ -9,13 +9,13 @@ namespace AdlClient.Models
         public double TimeDilation;
         public Dictionary<string, JobVertexProfile> VertexProfiles;
 
-        private static System.DateTime? _getDate(string dateString)
+        private static System.DateTime? ParseDate(string dateString)
         {
-            var dt = _getDate1(dateString);
+            var dt = _ParseDate1(dateString);
             return dt;
         }
 
-        private static System.DateTime? _getDate1(string dateString)
+        private static System.DateTime? _ParseDate1(string dateString)
         {
             dateString = dateString.Replace("PDT", "-8");
 
@@ -24,10 +24,10 @@ namespace AdlClient.Models
             bool result = DateTime.TryParseExact(dateString, fmt, System.Globalization.CultureInfo.InvariantCulture,
                 System.Globalization.DateTimeStyles.None, out dt);
 
-            return result ? (DateTime?) dt : _getDate2(dateString);
+            return result ? (DateTime?) dt : _ParseDate2(dateString);
         }
 
-        private static System.DateTime? _getDate2(string dateString)
+        private static System.DateTime? _ParseDate2(string dateString)
         {
             DateTime dt;
             dateString = dateString.Replace("PDT", "-8");
@@ -38,18 +38,17 @@ namespace AdlClient.Models
 
             return result ? (DateTime?)dt : null;
         }
-        public static DateTime DTMin(DateTime Date1, DateTime Date2)
+        public static DateTime DateTimeMin(DateTime d1, DateTime d2)
         {
-            return new DateTime(Math.Min(Date1.Ticks, Date2.Ticks));
+            return new DateTime(Math.Min(d1.Ticks, d2.Ticks));
         }
 
-        public static DateTime DTMax(DateTime Date1, DateTime Date2)
+        public static DateTime DateTimeMax(DateTime d1, DateTime d2)
         {
-            return new DateTime(Math.Max(Date1.Ticks, Date2.Ticks));
+            return new DateTime(Math.Max(d1.Ticks, d2.Ticks));
         }
 
-
-        private static string _fixId(string id)
+        private static string FixId(string id)
         {
             if (id.IndexOf('[') != -1)
             {
@@ -59,7 +58,6 @@ namespace AdlClient.Models
         }
 
         public static JobProfile Parse(string filename)
-
         {
             var profile = new JobProfile();
 
@@ -81,32 +79,32 @@ namespace AdlClient.Models
                 if (cols[0] == "timing")
                 {
                     var v = new JobVertexProfile();
-                    v.ApproxEndTime = _getDate(cols[1]);
-                    v.VersionCreatedTime = _getDate(cols[7]);
-                    v.VertexCreateStart = _getDate(cols[8]);
-                    v.VertexCreateEnd = _getDate(cols[12]);
-                    v.VertexQueueStart = _getDate(cols[19]);
-                    v.VertexQueueEnd = _getDate(cols[21]);
-                    v.ApproxEndTime = _getDate(cols[9]);
-                    v.VertexPNQueueEnd = _getDate(cols[20]);
-                    v.VertexStartTime = _getDate(cols[10]);
-                    v.VertexEndTime = _getDate(cols[22]);
-                    v.CleanedUpTime = _getDate(cols[11]);
+                    v.ApproxEndTime = ParseDate(cols[1]);
+                    v.VersionCreatedTime = ParseDate(cols[7]);
+                    v.VertexCreateStart = ParseDate(cols[8]);
+                    v.VertexCreateEnd = ParseDate(cols[12]);
+                    v.VertexQueueStart = ParseDate(cols[19]);
+                    v.VertexQueueEnd = ParseDate(cols[21]);
+                    v.ApproxEndTime = ParseDate(cols[9]);
+                    v.VertexPNQueueEnd = ParseDate(cols[20]);
+                    v.VertexStartTime = ParseDate(cols[10]);
+                    v.VertexEndTime = ParseDate(cols[22]);
+                    v.CleanedUpTime = ParseDate(cols[11]);
                     v.VertexGuid = cols[3];
                     v.ProcessId = cols[4];
-                    v.VertexId = _fixId(cols[4]);
+                    v.VertexId = FixId(cols[4]);
                     v.BytesRead = long.Parse(cols[13]);
                     v.BytesWritten = long.Parse(cols[14]);
                     v.VertexResult = cols[5];
 
                     if (v.VertexPNQueueEnd.HasValue && v.VersionCreatedTime.HasValue)
                     {
-                        startTime = DTMin(v.VersionCreatedTime.Value, startTime);
+                        startTime = DateTimeMin(v.VersionCreatedTime.Value, startTime);
                     }
 
                     if (v.VertexEndTime.HasValue && v.CleanedUpTime.HasValue)
                     {
-                        endTime = DTMax(v.CleanedUpTime.Value, endTime);
+                        endTime = DateTimeMax(v.CleanedUpTime.Value, endTime);
                     }
 
                     profile.VertexProfiles[v.VertexGuid] = v;
