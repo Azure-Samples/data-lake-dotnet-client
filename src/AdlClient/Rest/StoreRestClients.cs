59215b9d49074fc16a4771333fbbe88fcd4341a8
Saveen Reddy <saveenr@live.com>
2017-08-22 15:08:44 -0700

    Removed Rest wrapper classes

--- src/AdlClient/Rest/StoreRestClients.cs
+++ src/AdlClient/Rest/StoreRestClients.cs
@@ -1,22 +1,25 @@
 ﻿using Microsoft.Azure.Management.DataLake.Store;
+using MSADLS = Microsoft.Azure.Management.DataLake.Store;
 using ADL = Microsoft.Azure.Management.DataLake;
+
 namespace AdlClient.Rest
 {
     public class StoreRestClients
     {
         public readonly ADL.Store.DataLakeStoreFileSystemManagementClient FileSystem;
-        public readonly StoreManagementRestWrapper StoreAccountMgmtRest;
         public readonly AdlClient.Models.StoreAccountRef Account;
         public readonly Microsoft.Azure.Graph.RBAC.GraphRbacManagementClient AADclient;
+        public readonly MSADLS.DataLakeStoreAccountManagementClient AccountClient;
 
         public StoreRestClients(Authentication authSession, AdlClient.Models.StoreAccountRef account)
         {
             this.Account = account;
-            this.StoreAccountMgmtRest = new StoreManagementRestWrapper(account.SubscriptionId, authSession.ARMCreds);
             this.AADclient = new Microsoft.Azure.Graph.RBAC.GraphRbacManagementClient(authSession.AADCreds);
             this.AADclient.TenantID = authSession.Tenant;
 
             this.FileSystem = new DataLakeStoreFileSystemManagementClient(authSession.ADLCreds);
+            this.AccountClient = new DataLakeStoreAccountManagementClient(authSession.ARMCreds);
+            this.AccountClient.SubscriptionId = account.SubscriptionId;
         }
     }
 }
