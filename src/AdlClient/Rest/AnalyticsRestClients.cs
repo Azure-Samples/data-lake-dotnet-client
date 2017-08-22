c59544284e67059f45181bfd58868f1837e669cc
Saveen Reddy <saveenr@live.com>
2017-08-22 12:39:28 -0700

    Removed the job rest wrapper

--- src/AdlClient/Rest/AnalyticsRestClients.cs
+++ src/AdlClient/Rest/AnalyticsRestClients.cs
@@ -1,21 +1,23 @@
 ﻿using AdlClient.Rest;
-
+using MSADLA = Microsoft.Azure.Management.DataLake.Analytics;
 namespace AdlClient
 {
     public class AnalyticsRestClients
     {
-        public readonly AnalyticsJobsRestWrapper _JobRest;
         public readonly AnalyticsCatalogRestWrapper _CatalogRest;
         public readonly AnalyticsAccountManagmentRestWrapper _AdlaAccountMgmtRest;
         public readonly Microsoft.Azure.Graph.RBAC.GraphRbacManagementClient AADclient;
 
+        public readonly MSADLA.DataLakeAnalyticsJobManagementClient Jobs;
+
         public AnalyticsRestClients(Authentication authSession, AdlClient.Models.AnalyticsAccountRef account)
         {
-            this._JobRest = new AnalyticsJobsRestWrapper(authSession.ADLCreds);
             this._CatalogRest = new AnalyticsCatalogRestWrapper(authSession.ADLCreds);
             this._AdlaAccountMgmtRest = new AnalyticsAccountManagmentRestWrapper(account.SubscriptionId, authSession.ARMCreds);
             this.AADclient = new Microsoft.Azure.Graph.RBAC.GraphRbacManagementClient(authSession.AADCreds);
             this.AADclient.TenantID = authSession.Tenant;
+
+            this.Jobs = new MSADLA.DataLakeAnalyticsJobManagementClient(authSession.ADLCreds);
         }
     }
 }
