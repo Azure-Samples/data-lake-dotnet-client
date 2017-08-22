4211ef772679ea7f5ebf28621ddac3687b040626
Saveen Reddy <saveenr@live.com>
2017-08-22 15:02:02 -0700

    Removed obsolete class

--- src/AdlClient/Rest/StoreRestClients.cs
+++ src/AdlClient/Rest/StoreRestClients.cs
@@ -5,8 +5,6 @@ namespace AdlClient.Rest
     public class StoreRestClients
     {
         public readonly ADL.Store.DataLakeStoreFileSystemManagementClient FS;
-
-        public readonly StoreFileSystemRestWrapper FileSystemRest;
         public readonly StoreManagementRestWrapper StoreAccountMgmtRest;
         public readonly AdlClient.Models.StoreAccountRef Account;
         public readonly Microsoft.Azure.Graph.RBAC.GraphRbacManagementClient AADclient;
@@ -14,7 +12,6 @@ namespace AdlClient.Rest
         public StoreRestClients(Authentication authSession, AdlClient.Models.StoreAccountRef account)
         {
             this.Account = account;
-            this.FileSystemRest = new StoreFileSystemRestWrapper(authSession.ADLCreds);
             this.StoreAccountMgmtRest = new StoreManagementRestWrapper(account.SubscriptionId, authSession.ARMCreds);
             this.AADclient = new Microsoft.Azure.Graph.RBAC.GraphRbacManagementClient(authSession.AADCreds);
             this.AADclient.TenantID = authSession.Tenant;
